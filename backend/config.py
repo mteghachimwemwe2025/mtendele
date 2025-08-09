e7395556d7d5867189155884694f84c83a88f8b6
chimwemwe <mteghachimwemwewilliam@gmail.com>
2025-08-09 12:06:35 +0200

    Update database URI and secret key in Config

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,4 @@
 class Config:
-    SECRET_KEY = 'your-secret-key'  # Replace with a real secret
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://sql8794265:Mtegha123###@sql8.freesqldatabase.com:3306/sql8794265'
+    SECRET_KEY = 'your-secret-key'  # Replace this with a strong secret key
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://sql8794315:wAXtYhzQlF@sql8.freesqldatabase.com:3306/sql8794315'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
