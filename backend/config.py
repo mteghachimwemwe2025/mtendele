c80495decda141aa3cd127efbd7bee815c41420f
Chimwemwe Mtegha <mteghachimwemwewilliam@gmail.com>
2026-04-13 17:13:54 +0200

    Fix database config and update Render MySQL connection

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,8 @@
 import os
 
 class Config:
-    SECRET_KEY = 'your-secret-key'
+    SECRET_KEY = os.getenv("SECRET_KEY", "default-secret")
+
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
